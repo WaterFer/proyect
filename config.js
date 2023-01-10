f62cad77ae3c2859690c05bd62a4fd1d3410442f
Hector Fernandez <h4451477@gmail.com>
2023-01-10 15:18:58 -0400

    proyecto incompleto

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const MONGO_URI = process.env.NODE_ENV === 'production'
 
-? process.env.MONGO_URI_PROD
+? process.env.MONGO_URI
 : process.env.MONGO_URI_TEST
 
 const PORT = process.env.PORT || 3003;
